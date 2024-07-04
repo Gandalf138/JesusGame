4a6c5afaa41ec53202acce126bb51244fbc7ac52
Elanora Manson <git@elanora.lol>
2024-07-03 18:34:38 -0700

    refactor, add automatic build
    refactor to be more maintainable and extensible
    
    add build.yml for automatic multiplatform builds
    
    add multiplatform scripts for easy launch
    
    add README

---
+++ data/audio/__init__.py
@@ -0,0 +1,11 @@
+from ..util import resource_path
+
+musicfile = resource_path("./data/audio/Ameno.wav")
+
+hit_sound_files = [
+    resource_path("./data/audio/Aced.wav"),
+    resource_path("./data/audio/Crash.wav"),
+    resource_path("./data/audio/McOof.wav"),
+    resource_path("./data/audio/RbOof.wav"),
+    resource_path("./data/audio/Wilhelm.wav"),
+]
---
+++ data/util/__init__.py
@@ -0,0 +1,1 @@
+from .resource_path import resource_path
